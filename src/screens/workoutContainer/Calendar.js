16791c45d2a308eb861364f22de3fd75af159309
Freddie Sethi <freddiesethi@hotmail.com>
2023-01-11 00:29:53 +0000

    commit

--- src/screens/workoutContainer/Calendar.js
+++ src/screens/workoutContainer/Calendar.js
@@ -1,52 +1,28 @@
-import React, { useMemo, useState, useCallback, useEffect } from "react";
-import { StyleSheet, View, Switch, Text } from "react-native";
+import React, { useMemo, useState, useEffect } from "react";
+import { View } from "react-native";
 import { NewCalendarList } from "react-native-calendars";
 import { getWorkoutDays } from "../../data/firestopreRealTime";
+import { colours } from "../../routes";
 
 const initialDate = getTodaysDate();
 
 const NewCalendarScreen = () => {
-  const [selected, setSelected] = useState(initialDate);
-  const [horizontal, setHorizontal] = useState(false);
+  const [selected] = useState(initialDate);
   const [markedDates, setMarkedDates] = useState({});
 
   useEffect(() => {
     getWorkoutDaysFormatted(setMarkedDates);
   }, []);
-  const onValueChange = useCallback(
-    (value) => {
-      setHorizontal(value);
-    },
-    [horizontal]
-  );
-
-  const onDayPress = useCallback(
-    (day) => {
-      console.warn("dayPress: ", day);
-      setSelected(day.dateString);
-    },
-    [setSelected]
-  );
 
   const calendarProps = useMemo(() => {
     return {
       markedDates: markedDates,
-      onDayPress: onDayPress,
     };
-  }, [selected, markedDates, onDayPress]);
+  }, [selected, markedDates]);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.switchView}>
-        <Text style={styles.switchText}>Horizontal</Text>
-        <Switch value={horizontal} onValueChange={onValueChange} />
-      </View>
-      <NewCalendarList
-        key={Number(horizontal)}
-        horizontal={horizontal}
-        staticHeader
-        calendarProps={calendarProps}
-      />
+    <View style={{ flex: 1 }}>
+      <NewCalendarList staticHeader calendarProps={calendarProps} />
     </View>
   );
 };
@@ -60,7 +36,7 @@ function getWorkoutDaysFormatted(setMarkedDates) {
     workoutDays.forEach((date) => {
       formattedData[date] = {
         selected: true,
-        selectedColor: "rgb(0, 200, 254)",
+        selectedColor: colours.lightBlue,
       };
     });
     setMarkedDates(formattedData);
@@ -76,26 +52,3 @@ function getTodaysDate() {
   const day = date.getDate();
   return `${year}-${month}-${day}`;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  switchView: {
-    flexDirection: "row",
-    height: 70,
-    padding: 10,
-    paddingBottom: 30,
-    backgroundColor: "white",
-    alignItems: "center",
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    left: 0,
-    zIndex: 100,
-  },
-  switchText: {
-    marginRight: 20,
-    fontSize: 16,
-  },
-});
